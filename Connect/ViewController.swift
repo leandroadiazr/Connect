b254f0492ae352ec1357ce5debb8b10b8a54065f
Leandro Diaz <leandroadiazr@gmail.com>
2021-01-29 20:07:03 -0500

    Initial Commit

---
+++ Connect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Connect
+//
+//  Created by Leandro Diaz on 1/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
